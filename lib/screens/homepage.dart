1ce5d211ce5ad1cce11c5165a7d6433ca135f9be
netesh5 <netesh.paudel@gmail.com>
2022-04-05 22:25:32 +0545

    Homepage_UI

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -5,11 +5,101 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: const Text(
           "Yoga App",
           style: TextStyle(color: Colors.black),
         ),
       ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text("1"),
+                      Text("Streak"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text("1"),
+                      Text("Streak"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text("1"),
+                      Text("Streak"),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Divider(
+                thickness: 2,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Yoga for all"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amberAccent,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amberAccent,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text("Yoga for students"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amberAccent,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amberAccent,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
