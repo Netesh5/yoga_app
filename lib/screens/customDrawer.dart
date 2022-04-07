d2e3d5fa52bb8570a0131ddd5756bd070b0c7bd2
netesh5 <netesh.paudel@gmail.com>
2022-04-07 19:25:01 +0545

    drawer_items

--- lib/screens/customDrawer.dart
+++ lib/screens/customDrawer.dart
@@ -5,12 +5,36 @@ class customDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          Image.network(
-              'https://images.unsplash.com/photo-1593810450979-79e8584f76b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80'),
-        ],
+    return SafeArea(
+      child: Drawer(
+        child: Column(
+          children: [
+            Image.network(
+                'https://images.unsplash.com/photo-1593810450979-79e8584f76b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80'),
+            SizedBox(
+              height: 10,
+            ),
+            Divider(
+              thickness: 2,
+            ),
+            ListTile(
+              title: Text("Reset Progress"),
+              leading: Icon(Icons.restart_alt_outlined),
+            ),
+            ListTile(
+              title: Text("Share with Friends"),
+              leading: Icon(Icons.share),
+            ),
+            ListTile(
+              title: Text("Rate us"),
+              leading: Icon(Icons.star),
+            ),
+            ListTile(title: Text("Feedback"), leading: Icon(Icons.feedback)),
+            ListTile(
+                title: Text("Privacy Policy"),
+                leading: Icon(Icons.privacy_tip_outlined)),
+          ],
+        ),
       ),
     );
   }
