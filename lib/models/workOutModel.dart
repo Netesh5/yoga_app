0e13c8255c00f96a2886a51425699f55a1e5b542
netesh5 <netesh.paudel@gmail.com>
2022-04-09 00:45:42 +0545

    workout_page

--- lib/models/workOutModel.dart
+++ lib/models/workOutModel.dart
@@ -1,5 +1,19 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 
 class workOutModel with ChangeNotifier {
+  workOutModel() {
+    Mytimer();
+  }
   int countdown = 30;
+  Mytimer() async {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      countdown--;
+      notifyListeners();
+      if (countdown == 0) {
+        timer.cancel();
+      }
+    });
+  }
 }
--- lib/screens/workout.dart
+++ lib/screens/workout.dart
@@ -10,8 +10,82 @@ class workOut extends StatelessWidget {
     return ChangeNotifierProvider<workOutModel>(
       create: (context) => workOutModel(),
       child: Scaffold(
-        body: Container(
-          child: Text("Start Yoga"),
+        body: Column(
+          children: [
+            Container(
+              height: 250,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          "https://img.freepik.com/free-vector/illustration-vector-graphic-sunset-yoga-silhouette-perfect-use-yoga-studio-wallpaper_315140-83.jpg"))),
+            ),
+            Spacer(),
+            Text("Yoga",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Container(
+              height: 50,
+              width: 150,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurpleAccent,
+                  borderRadius: BorderRadius.circular(8)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("00",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                  Text(" : ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                  Consumer<workOutModel>(
+                      builder: (context, value, child) => Text(
+                          "${value.countdown}",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)))
+                ],
+              ),
+            ),
+            Spacer(),
+            Container(
+              height: 50,
+              width: 150,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurpleAccent,
+                  borderRadius: BorderRadius.circular(8)),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Pause",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20))),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Previous",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Next",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
